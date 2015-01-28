bc3561ce1586d3125f9f3b3e2d08026c28f316bb
Jason Coon <jasoncoon@hotmail.com>
2015-01-27 20:35:14 -0600

    Reformatted code, fixed a few issues

--- BlinkyTileFastLED.ino
+++ BlinkyTileFastLED.ino
@@ -3,58 +3,144 @@
 
 #define LED_PIN     0
 #define NUM_LEDS    12
-#define BRIGHTNESS  64
+#define BRIGHTNESS  255
 #define LED_TYPE    DMXSIMPLE
 #define COLOR_ORDER BGR
 CRGB leds[NUM_LEDS];
 
 void setup() {
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-    FastLED.setCorrection(TypicalLEDStrip);
-    FastLED.setBrightness(BRIGHTNESS);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
 }
 
 byte hue = 0;
 byte currentLed = 0;
 
 void loop() {
-    uint16_t delay = 0;
+  uint16_t delay = 0;
 
-    // delay = solidHueShift();
-    // delay = sequenceHueShift();
-    delay = colorPaletteExample();
+  // delay = solidHueShift();
+  // delay = sequenceHueShift();
+  // delay = antiAliasedLightBar();
+  delay = colorPaletteExample();
 
-    FastLED.show();
-    FastLED.delay(delay);
+  FastLED.show();
+  FastLED.delay(delay);
 }
 
 uint16_t solidHueShift() {
-    fill_solid(leds, NUM_LEDS, CHSV(hue, 255, 255));
-    hue++;
-    delay(30);
+  fill_solid(leds, NUM_LEDS, CHSV(hue, 255, 255));
+  hue++;
+  return 30;
 }
 
 uint16_t sequenceHueShift() {
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-    leds[currentLed] = CHSV(hue, 255, 255);
-    hue++;
-    currentLed++;
-    if (currentLed >= NUM_LEDS) {
-        currentLed = 0;
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  leds[currentLed] = CHSV(hue, 255, 255);
+  hue++;
+  currentLed++;
+  if (currentLed >= NUM_LEDS) {
+    currentLed = 0;
+  }
+  return 120;
+}
+
+int     F16pos = 0; // position of the "fraction-based bar"
+int     F16delta = 1; // how many 16ths of a pixel to move the Fractional Bar
+uint16_t Fhue16 = 0; // color for Fractional Bar
+
+int Width  = 2; // width of each light bar, in whole pixels
+
+uint16_t antiAliasedLightBar() {
+  // Update the "Fraction Bar" by 1/16th pixel every time
+  F16pos += F16delta;
+
+  // wrap around at end
+  // remember that F16pos contains position in "16ths of a pixel"
+  // so the 'end of the strip' is (NUM_LEDS * 16)
+  if( F16pos >= (NUM_LEDS * 16)) {
+    F16pos -= (NUM_LEDS * 16);
+  }
+
+  // Draw everything:
+  // clear the pixel buffer
+  memset8( leds, 0, NUM_LEDS * sizeof(CRGB));
+
+  // advance to the next hue
+  Fhue16 = Fhue16 + 19;
+
+  // draw the Fractional Bar, length=4px
+  drawFractionalBar( F16pos, Width, Fhue16 / 256);
+
+  return 40;
+}
+
+// Draw a "Fractional Bar" of light starting at position 'pos16', which is counted in
+// sixteenths of a pixel from the start of the strip.  Fractional positions are
+// rendered using 'anti-aliasing' of pixel brightness.
+// The bar width is specified in whole pixels.
+// Arguably, this is the interesting code.
+void drawFractionalBar( int pos16, int width, uint8_t hue)
+{
+  int i = pos16 / 16; // convert from pos to raw pixel number
+  uint8_t frac = pos16 & 0x0F; // extract the 'factional' part of the position
+
+  // brightness of the first pixel in the bar is 1.0 - (fractional part of position)
+  // e.g., if the light bar starts drawing at pixel "57.9", then
+  // pixel #57 should only be lit at 10% brightness, because only 1/10th of it
+  // is "in" the light bar:
+  //
+  //                       57.9 . . . . . . . . . . . . . . . . . 61.9
+  //                        v                                      v
+  //  ---+---56----+---57----+---58----+---59----+---60----+---61----+---62---->
+  //     |         |        X|XXXXXXXXX|XXXXXXXXX|XXXXXXXXX|XXXXXXXX |  
+  //  ---+---------+---------+---------+---------+---------+---------+--------->
+  //                   10%       100%      100%      100%      90%        
+  //
+  // the fraction we get is in 16ths and needs to be converted to 256ths,
+  // so we multiply by 16.  We subtract from 255 because we want a high
+  // fraction (e.g. 0.9) to turn into a low brightness (e.g. 0.1)
+  uint8_t firstpixelbrightness = 255 - (frac * 16);
+
+  // if the bar is of integer length, the last pixel's brightness is the
+  // reverse of the first pixel's; see illustration above.
+  uint8_t lastpixelbrightness  = 255 - firstpixelbrightness;
+
+  // For a bar of width "N", the code has to consider "N+1" pixel positions,
+  // which is why the "<= width" below instead of "< width".
+
+  uint8_t bright;
+  for( int n = 0; n <= width; n++) {
+    if( n == 0) {
+      // first pixel in the bar
+      bright = firstpixelbrightness;
+    } 
+    else if( n == width ) {
+      // last pixel in the bar
+      bright = lastpixelbrightness;
+    } 
+    else {
+      // middle pixels
+      bright = 255;
     }
-    delay(120);
+
+    leds[i] += CHSV( hue, 255, bright);
+    i++;
+    if( i == NUM_LEDS) i = 0; // wrap around
+  }
 }
 
 // ColorPalette example by Mark Kriegsman: https://github.com/FastLED/FastLED/blob/master/examples/ColorPalette/ColorPalette.ino
 uint16_t colorPaletteExample() {
-    ChangePalettePeriodically();
+  ChangePalettePeriodically();
 
-    static uint8_t startIndex = 0;
-    startIndex = startIndex + 1; /* motion speed */
+  static uint8_t startIndex = 0;
+  startIndex++; /* motion speed */
 
-    FillLEDsFromPaletteColors(startIndex);
+  FillLEDsFromPaletteColors(startIndex);
 
-    return 10;
+  return 10;
 }
 
 // This example shows several ways to set up and use 'palettes' of colors
@@ -82,12 +168,12 @@ extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 
 void FillLEDsFromPaletteColors(uint8_t colorIndex)
 {
-    uint8_t brightness = 255;
+  uint8_t brightness = 255;
 
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette(currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(currentPalette, colorIndex, brightness, currentBlending);
+    colorIndex += 3;
+  }
 }
 
 
@@ -101,31 +187,64 @@ void FillLEDsFromPaletteColors(uint8_t colorIndex)
 
 void ChangePalettePeriodically()
 {
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
+  uint8_t secondHand = (millis() / 1000) % 60;
+  static uint8_t lastSecond = 99;
 
-    if (lastSecond != secondHand) {
-        lastSecond = secondHand;
-        if (secondHand == 0)  { currentPalette = RainbowColors_p;         currentBlending = BLEND; }
-        if (secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND; }
-        if (secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = BLEND; }
-        if (secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = BLEND; }
-        if (secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = BLEND; }
-        if (secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if (secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = BLEND; }
-        if (secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = BLEND; }
-        if (secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = BLEND; }
-        if (secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND; }
-        if (secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = BLEND; }
+  if (lastSecond != secondHand) {
+    lastSecond = secondHand;
+    if (secondHand == 0)  { 
+      currentPalette = RainbowColors_p;         
+      currentBlending = BLEND; 
     }
+    if (secondHand == 10)  { 
+      currentPalette = RainbowStripeColors_p;   
+      currentBlending = NOBLEND; 
+    }
+    if (secondHand == 15)  { 
+      currentPalette = RainbowStripeColors_p;   
+      currentBlending = BLEND; 
+    }
+    if (secondHand == 20)  { 
+      SetupPurpleAndGreenPalette();             
+      currentBlending = BLEND; 
+    }
+    if (secondHand == 25)  { 
+      SetupTotallyRandomPalette();              
+      currentBlending = BLEND; 
+    }
+    if (secondHand == 30)  { 
+      SetupBlackAndWhiteStripedPalette();       
+      currentBlending = NOBLEND; 
+    }
+    if (secondHand == 35)  { 
+      SetupBlackAndWhiteStripedPalette();       
+      currentBlending = BLEND; 
+    }
+    if (secondHand == 40)  { 
+      currentPalette = CloudColors_p;           
+      currentBlending = BLEND; 
+    }
+    if (secondHand == 45)  { 
+      currentPalette = PartyColors_p;           
+      currentBlending = BLEND; 
+    }
+    if (secondHand == 50)  { 
+      currentPalette = myRedWhiteBluePalette_p; 
+      currentBlending = NOBLEND; 
+    }
+    if (secondHand == 55)  { 
+      currentPalette = myRedWhiteBluePalette_p; 
+      currentBlending = BLEND; 
+    }
+  }
 }
 
 // This function fills the palette with totally random colors.
 void SetupTotallyRandomPalette()
 {
-    for (int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV(random8(), 255, random8());
-    }
+  for (int i = 0; i < 16; i++) {
+    currentPalette[i] = CHSV(random8(), 255, random8());
+  }
 }
 
 // This function sets up a palette of black and white stripes,
@@ -134,28 +253,28 @@ void SetupTotallyRandomPalette()
 // to set them up.
 void SetupBlackAndWhiteStripedPalette()
 {
-    // 'black out' all 16 palette entries...
-    fill_solid(currentPalette, 16, CRGB::Black);
-    // and set every fourth one to white.
-    currentPalette[0] = CRGB::White;
-    currentPalette[4] = CRGB::White;
-    currentPalette[8] = CRGB::White;
-    currentPalette[12] = CRGB::White;
+  // 'black out' all 16 palette entries...
+  fill_solid(currentPalette, 16, CRGB::Black);
+  // and set every fourth one to white.
+  currentPalette[0] = CRGB::White;
+  currentPalette[4] = CRGB::White;
+  currentPalette[8] = CRGB::White;
+  currentPalette[12] = CRGB::White;
 
 }
 
 // This function sets up a palette of purple and green stripes.
 void SetupPurpleAndGreenPalette()
 {
-    CRGB purple = CHSV(HUE_PURPLE, 255, 255);
-    CRGB green = CHSV(HUE_GREEN, 255, 255);
-    CRGB black = CRGB::Black;
+  CRGB purple = CHSV(HUE_PURPLE, 255, 255);
+  CRGB green = CHSV(HUE_GREEN, 255, 255);
+  CRGB black = CRGB::Black;
 
-    currentPalette = CRGBPalette16(
-        green, green, black, black,
-        purple, purple, black, black,
-        green, green, black, black,
-        purple, purple, black, black);
+  currentPalette = CRGBPalette16(
+  green, green, black, black,
+  purple, purple, black, black,
+  green, green, black, black,
+  purple, purple, black, black);
 }
 
 
@@ -165,24 +284,24 @@ void SetupPurpleAndGreenPalette()
 // takes up 64 bytes of flash.
 const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
 {
-    CRGB::Red,
-    CRGB::Gray, // 'white' is too bright compared to red and blue
-    CRGB::Blue,
-    CRGB::Black,
+  CRGB::Red,
+  CRGB::Gray, // 'white' is too bright compared to red and blue
+  CRGB::Blue,
+  CRGB::Black,
 
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Black,
+  CRGB::Red,
+  CRGB::Gray,
+  CRGB::Blue,
+  CRGB::Black,
 
-    CRGB::Red,
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Blue,
-    CRGB::Black,
-    CRGB::Black
+  CRGB::Red,
+  CRGB::Red,
+  CRGB::Gray,
+  CRGB::Gray,
+  CRGB::Blue,
+  CRGB::Blue,
+  CRGB::Black,
+  CRGB::Black
 };
 
 
@@ -208,3 +327,8 @@ const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
 // palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
 // the first sixteen entries from the virtual palette (of 256), you'd get
 // Green, followed by a smooth gradient from green-to-blue, and then Blue.
+
+
+
+
+
